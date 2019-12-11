58be449c628b6201c9366ba7aa0e522e6435dcfd
Txclever <Txclever.com>
2019-12-11 09:56:16 +0800

    arm

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 window.onscroll = function () {
     var top = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
     var node = document.getElementById('p-left-o');
-    if (top > 100) {//500就是滚动条滚动到的位置，大于500才显示
+    if (top > 100) {
         node.style.display = 'block';
     } else {
         node.style.display = 'none';
